18248cba82b749527aaa47f634e2ff8b4b388fbd
Brian-Rono <b.rono.br@gmail.com>
2023-07-30 14:21:20 +0300

    Update to fetch the links from the content script.

--- popup.js
+++ popup.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Sample links, you can replace these with your own list of links.
-  const links = [
-    "https://www.example.com",
-    "https://www.example.org",
-    "https://www.example.net"
-  ];
-
   const linkList = document.getElementById("link-list");
   const openSelectedBtn = document.getElementById("open-selected-btn");
 
-  links.forEach((link) => {
-    const listItem = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
+  // Request content script to fetch links from the current tab.
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const tab = tabs[0];
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: getLinks,
+    });
+  });
 
-    listItem.appendChild(checkbox);
-    listItem.appendChild(document.createTextNode(link));
+  // Listen for messages from content script.
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "sendLinks") {
+      const links = request.links || [];
+      links.forEach((link) => {
+        const listItem = document.createElement("li");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
 
-    linkList.appendChild(listItem);
+        listItem.appendChild(checkbox);
+        listItem.appendChild(document.createTextNode(link));
+
+        linkList.appendChild(listItem);
+      });
+    }
   });
 
   openSelectedBtn.addEventListener("click", function () {
@@ -25,8 +33,14 @@ document.addEventListener("DOMContentLoaded", function () {
     checkboxes.forEach((checkbox, index) => {
       if (checkbox.checked) {
         // Open the link in a new tab.
-        window.open(links[index], "_blank");
+        window.open(checkbox.nextSibling.textContent, "_blank");
       }
     });
   });
 });
+
+// Content script to extract all links from the DOM and send them back to popup.
+function getLinks() {
+  const links = Array.from(document.querySelectorAll("a")).map((a) => a.href);
+  chrome.runtime.sendMessage({ action: "sendLinks", links: links });
+}
